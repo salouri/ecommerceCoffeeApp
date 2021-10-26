d57fa5bb481847349dd326917213d597c0cf610f
Salouri Developer <salouri@gmail.com>
2021-10-26 22:04:11 +0300

    add essential routes

--- app/v1/routes/coffeeMachineRoutes.js
+++ app/v1/routes/coffeeMachineRoutes.js
@@ -8,5 +8,10 @@ const router = express.Router();
 //All below Routes are protected
 router.use(authController.isAuthorized);
 
+// Remember: '/' ==>> '/api/v1/coffee-machines/'
+router
+  .route('/')
+  .get(machineController.getAllMachines)
+  .post(machineController.createMachine);
 //
 module.exports = router;
--- app/v1/routes/coffeePodRoutes.js
+++ app/v1/routes/coffeePodRoutes.js
@@ -8,5 +8,7 @@ const router = express.Router();
 //All below Routes are protected
 router.use(authController.isAuthorized);
 
+// Remember: '/' ==>> '/api/v1/coffee-pods/'
+router.route('/').get(podController.getAllPods).post(podController.createPod);
 //
 module.exports = router;
